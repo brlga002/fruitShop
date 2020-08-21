51b178ee5fef2f8f3d26a8b35b705a923a12788b
brlga002 <gabriel.lima1000@gmail.com>
2020-08-21 16:55:44 -0400

    feat add webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,36 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.
+    styles([
+        'resources/views/dash/vendors/bootstrap/dist/css/bootstrap.min.css',
+        'resources/views/dash/vendors/font-awesome/css/font-awesome.min.css',
+        'resources/views/dash/vendors/themify-icons/css/themify-icons.css',
+        'resources/views/dash/vendors/flag-icon-css/css/flag-icon.min.css',
+        'resources/views/dash/vendors/selectFX/css/cs-skin-elastic.css',
+        'resources/views/dash/vendors/jqvmap/dist/jqvmap.min.css',
+        'resources/views/dash/assets/css/style.css',
+    ],'public/dash/assets/css/style.css')
+
+    .options({
+        processCssUrls: false
+    })
+
+    .scripts([
+        'resources/views/dash/vendors/jquery/dist/jquery.min.js',
+        'resources/views/dash/vendors/popper.js/dist/umd/popper.min.js',
+        'resources/views/dash/vendors/bootstrap/dist/js/bootstrap.min.js',
+        'resources/views/dash/assets/js/main.js',
+        'resources/views/dash/vendors/chart.js/dist/Chart.bundle.min.js',
+        'resources/views/dash/assets/js/dashboard.js',
+        'resources/views/dash/assets/js/widgets.js',
+        'resources/views/dash/vendors/jqvmap/dist/jquery.vmap.min.js',
+        'resources/views/dash/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js',
+        'resources/views/dash/vendors/jqvmap/dist/maps/jquery.vmap.world.js',
+    ],'public/dash/assets/js/script.js')
+
+    .copy('resources/views/dash/images', 'public/dash/images')
+    .copy('resources/views/dash/vendors/font-awesome/fonts', 'public/dash/assets/fonts')
+    .copy('resources/views/dash/vendors/themify-icons/fonts', 'public/dash/assets/fonts')
+
+   .version()
